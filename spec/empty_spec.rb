4cdc3b47d5bc3eddb9848e231e297915934e1640
Tony Godwin <ajgodwin@gmail.com>
2019-03-04 11:09:37 -0600

    prep for class 11

---
+++ spec/empty_spec.rb
@@ -0,0 +1,10 @@
+
+describe "As a user I must have email to register" do
+	
+    it 'Given I try to register'
+    
+    it 'When I do not input an email' 
+    
+    it 'Then I get an error message' 
+   
+end
