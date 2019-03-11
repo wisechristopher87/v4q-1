2e935699a9f93e469dc0417272f53c8b49f3b444
Tony Godwin <ajgodwin@gmail.com>
2019-03-11 10:51:34 -0500

    new stub

---
+++ spec/user_can_login_spec.rb
@@ -0,0 +1,12 @@
+
+describe "As a user I can login" do
+	
+    it 'Given I open the application'
+    
+    it 'When I click the Make Appointment button'
+    
+    it 'and I enter my credentials' 
+    
+    it 'Then I see the Make Appointment page' 
+   
+end
