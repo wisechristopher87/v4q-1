c1810f84d8060246abbac00c5761bf565d947fed
Tony Godwin <ajgodwin@gmail.com>
2020-02-14 20:41:15 -0600

    had to look them up

--- spec/user_can_login_spec.rb
+++ spec/user_can_login_spec.rb
@@ -1,4 +1,3 @@
-
 describe "A user can login and make an appointment" do
 
 	    it 'Given I want to make an appointment'
@@ -7,11 +6,11 @@ describe "A user can login and make an appointment" do
 
 	    it 'and I enter a valid password'
 
-			it 'When I click the ??? button'
+			it 'When I click the Login button'
 
 			it 'and I enter my appointment information'
 
-			it 'and I click the ?? button'
+			it 'and I click the Book Appointment button'
 
 	    it 'Then I get a confirmation page'
 end
